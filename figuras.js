7c4ea8135b6a1edf7ea3817110f3d2eeee8ae896
sebastian <jsvaleroa@gmail.com>
2022-01-05 01:58:28 -0500

    cambios en las funciones y nueva interfaz interactiva en html

--- figuras.js
+++ figuras.js
@@ -1,1 +1,36 @@
-console.log("Hola");
+
+// codigo del cuadrado
+function perimetroCuadrado(value){
+    return value*4;
+}
+
+function areaCuadrado(value){
+    return value*value;
+}
+
+function perimetroCirculo(value){
+    const PI = Math.PI;
+    return value*PI;
+}
+
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function CalcularAreaCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const Area = areaCuadrado(value);
+    alert(Area);
+}
+
+function calcularCircunferencia(){
+    const input = document.getElementById("inputCirculo");
+    const value = input.value;
+    const circunferencia = perimetroCirculo(value);
+    alert(circunferencia);
+}
